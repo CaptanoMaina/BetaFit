5dea127ca7a102771a575756260d055026b5429e
Captano Maina <captanomaina@gmail.com>
2021-12-09 22:31:44 +0400

    fixed google authentication

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword =
+  "mongodb+srv://ciku:" +
+  encodeURIComponent("Wanjiku101") +
+  "@cluster0.ocpvp.mongodb.net/betafit?retryWrites=true&w=majority";
+
+module.exports = {
+  mongoURI: dbPassword,
+};
